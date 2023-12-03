fa923b28cab2230e4d30169f5edab84c49df4e89
Leosabinghimire <sabinghimire2055@gmail.com>
2023-12-03 14:24:27 +0545

    quiz-app

---
+++ src/assets/data.js
@@ -0,0 +1,97 @@
+export const data = [
+  {
+    question: "Jamie Vardy was signed by Leicester from which non-league side?",
+    option1: "ketting Town",
+    option2: "Alfreton Town",
+    option3: "Grimsby Town",
+    option4: "Fleetwood Town",
+    ans: 4,
+  },
+
+  {
+    question: "What was the score in the Euro 2012 final?",
+    option1: "2-0",
+    option2: "3-0",
+    option3: "4-0",
+    option4: "5-0",
+    ans: 3,
+  },
+
+  {
+    question:
+      " Football Player Quiz: Who won the Man of the Match award in the 2014 World Cup final?",
+    option1: "Mario Goetze",
+    option2: "Sergio Aguero",
+    option3: "Lionel Messi",
+    option4: "Bastian Schweinsteiger",
+    ans: 1,
+  },
+
+  {
+    question:
+      "Against which country did Wayne Rooney break the England goalscoring record?",
+    option1: "Switzerland",
+    option2: "San Marino",
+    option3: "Lithuania",
+    option4: "Slovenia",
+    ans: 1,
+  },
+
+  {
+    question: "Which team has won the African Cup of Nations a record 7 times?",
+    option1: "Cameroon",
+    option2: "Egypt",
+    option3: "Senegal",
+    option4: "Ghana",
+    ans: 2,
+  },
+
+  {
+    question:
+      "You can find the oldest footballer in the world playing in which country?",
+    option1: "Malaysia",
+    option2: "Ecuador",
+    option3: "Japan",
+    option4: "South Africa",
+    ans: 3,
+  },
+
+  {
+    question:
+      " Chelsea beat which team 8-0 to secure the 2009-10 Premier League title on the final day of the season?",
+    option1: "Blackburn",
+    option2: "Hull",
+    option3: "Wigan",
+    option4: "Norwich",
+    ans: 3,
+  },
+
+  {
+    question: " Which team did Porto beat in the 2004 Champions League final?",
+    option1: "Bayern Munich",
+    option2: "Deportivo La Coruna",
+    option3: "Barcelona",
+    option4: "Monaco",
+    ans: 4,
+  },
+
+  {
+    question:
+      " David Beckham became president of which newly founded club in 2018?",
+    option1: "Bergamo Calcio",
+    option2: "Inter Miami",
+    option3: "West London Blue",
+    option4: "The Potteries",
+    ans: 2,
+  },
+
+  {
+    question:
+      " In 2011, a 5th tier match in Argentina saw a record number of red cards. How many were given out?",
+    option1: "6",
+    option2: "11",
+    option3: "22",
+    option4: "36",
+    ans: 4,
+  },
+];
